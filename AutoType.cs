3edd0c6358f13b5d34c43be0b9a7b684623d4bee
safi32272 <61039693+safi32272@users.noreply.github.com>
2022-01-25 10:49:39 +0500

    Auto type a text

---
+++ AutoType.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AutoType : MonoBehaviour
+{
+    public float letterPause = 0.02f;
+    public AudioClip sound;
+    public AudioSource audio;
+    public string message;
+    public GameObject okBt;
+    private void Start()
+    {
+        if (okBt)
+        {
+            okBt.SetActive(false);
+        }
+
+        message = GetComponent<Text>().text;
+        this.GetComponent<Text>().text = "";
+        //GetComponent<AudioSource>().Play();
+
+        StartCoroutine(TypeText());
+    }
+    // Use this for initialization
+    void OnEnable()
+    {
+        
+        //if (sound)
+    }
+     
+    IEnumerator TypeText()
+    {
+        foreach (char letter in message.ToCharArray())
+        {
+            GetComponent<Text>().text += letter;
+            if (sound)
+                audio.PlayOneShot(sound);
+            // yield return 0;
+            yield return new WaitForSeconds(letterPause);
+        }
+        if(okBt)
+        {
+            okBt.SetActive(true);
+        }
+        
+    }
+}
